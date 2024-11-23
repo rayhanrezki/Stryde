5d59425ca0735d97eb332df094e2979c82d82d41
RafiAkem <yc66zio@gmail.com>
2024-11-24 03:24:44 +0700

    Updated Login UI

--- resources/js/Pages/Main.tsx
+++ resources/js/Pages/Main.tsx
@@ -3,16 +3,20 @@ import Hero from "../Components/Hero";
 import NewDrops from "../Components/NewDrops";
 import Categories from "@/Components/Categories";
 import Reviews from "@/Components/Reviews";
+import { Head } from "@inertiajs/react";
 
 function Main() {
     return (
-        <div>
-            <Navbar />
-            <Hero />
-            <NewDrops />
-            <Categories />
-            <Reviews />
-        </div>
+        <>
+            <Head title="Home" />
+            <div>
+                <Navbar />
+                <Hero />
+                <NewDrops />
+                <Categories />
+                <Reviews />
+            </div>
+        </>
     );
 }
 
