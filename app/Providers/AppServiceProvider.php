93335f343e10b9d35b4e3006bcbbcfa9ab7b3ce1
RafiAkem <yc66zio@gmail.com>
2024-12-31 12:31:19 +0700

    fix htaccess

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use App\Http\Middleware\IsAdmin;
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,5 +25,8 @@ class AppServiceProvider extends ServiceProvider
     {
         Vite::prefetch(concurrency: 3);
         $this->app['router']->aliasMiddleware('IsAdmin', IsAdmin::class);
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
